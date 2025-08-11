fde8cedcb9a292418471d8fe40ae53ee89c35094
Paulo Victor Gomes <pv.gomes89@gmail.com>
2025-08-11 18:46:40 +0200

    eventually vite import

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { serveStatic, log } from "./vite";
+// Vite will only be imported in development
 
 const app = express();
 app.use(express.json());
@@ -51,6 +52,8 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
+    // Dynamically import Vite setup only in development
+    const { setupVite } = await import("./vite");
     await setupVite(app, server);
   } else {
     serveStatic(app);
